0fcb3e09918be1e2d41f993a3f0fb059702b53cb
Bludidi <bludidi5@gmail.com>
2023-01-13 21:41:27 +0200

    Update person.rb file

--- Entities/person.rb
+++ Entities/person.rb
@@ -1,21 +1,30 @@
-class Person
+require_relative '../Decorator/nameable'
+
+class Person < Nameable
   attr_reader :id
   attr_accessor :name, :age
 
-  def initilialize(age, name: 'Unknown', parent_permission: true)
-    @id = Random.rand(1..100)
+  def initialize(age, name: 'Unknown', parent_permission: true)
+    super()
+    @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
-  end
-
-  def can_use_services?
-    of_age? || @parent_permission
   end
 
   private
 
   def of_age?
     @age >= 18
+  end
+
+  public
+
+  def can_use_services?
+    of_age? || @parent_permission
+  end
+
+  def correct_name
+    @name
   end
 end
